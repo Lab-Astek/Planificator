0d5ea37d3a88579c3687b696c8b01e6a21c8844c
Dowwind <romain.deffains@epitech.eu>
2019-12-12 15:58:40 +0100

    #1 - premiere version de la home page. c'est surtout du setup de l'architecture front

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: "MainPage",
+
+  data() {
+    return {
+      msg: "This is the Home page !"
+    };
+  }
+};
+</script>
+
+<style scoped />
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import MainPage from "@/components/MainPage";
+
+export default {
+  name: "home",
+  components: {
+    MainPage
+  }
+};
+</script>
